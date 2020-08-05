e12b5bf8cbbe1025a9e283230fd224e8caa979ec
BipZ143 <sahoobhanupratap2@gmail.com>
2020-08-05 13:37:37 +0530

    loop Flip Coin

---
+++ loopToss.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+numOfTosses=0
+numOfHeads=0
+numOfTails=0
+while [ $numOfTosses -lt 10]
+do
+	tossOutCome=$((RANDOM%2))
+	if [ $tossoutCome -eq 0 ]
+	then
+		echo "Heads"
+		((numOfHeads++))
+		((numOfTosses++))
+	else
+		echo "Tails"
+		((numOfTails++))
+		((numOfTosses++))
+	fi
+done
+echo "Number of heads: $numOfHeads Number of tails: $numOfTails"
